f4b0f1df3da3349f20a3b77d395263b65e1c249d
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-20 16:02:28 +0530

    Create p94.c

---
+++ p94.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+ int main()
+  {
+          int a[10],k,N,i;
+          printf("Enter the two numbers:");
+          scanf("%d%d",&N,&k);
+          for(i=1;i<N;i++)
+          scanf("%d",&a[i]);
+          printf("The 'K'th number is :%d",a[k]);
+          return o;
+   }
